e3095c0142076ddf59357d3412f2005080097438
AlexC-ux <eunv2004@gmail.com>
2022-08-27 22:03:43 +0300

    123

--- src/views/view.tsx
+++ src/views/view.tsx
@@ -6,6 +6,8 @@ function ReactComponent() {
 
     document.body.classList.add("bg-dark")
 
+    let publicPath = process.env.PUBLIC_URL;
+
     return (<>
         <div className="w-100 bg-dark titlecontainer text-light" style={{ height: `${document.documentElement.clientHeight}px` }}>
             <div className="row g-0 justify-content-center" style={{ paddingTop: `${(document.documentElement.clientHeight / 100 * 60)}px`, bottom: "0px" }}>
@@ -84,14 +86,14 @@ function ReactComponent() {
         <div className="container bg-dark text-light mt-5">
             <h2 className="text-center border-bottom my-2 pb-2 mx-auto" style={{ maxWidth: "250px" }}>Проекты</h2>
 
-            <Card theme="red" time={new Date()} tag1=<div>TSX + Node.Js</div> tag2=<a href="https://web.archive.org/web/20220827154459/https://xn--80aerhcl3be.xn--p1ai/"><i className="bi bi-box-arrow-up-right mx-2"></i>WebArchive</a> link="//активмкт.рф" linkText="Открыть сайт" title="Сайт студентческого актива МКТ" imageLink="/images/products/activemkt.png"
+            <Card theme="red" time={new Date("08 27 2022")} link="//github.com/AlexC-ux" linkText="Открыть GitHub" title="Портфолио" imageLink={`${publicPath}/images/products/activemkt.png` }
                 text=<div>Сайт, разработанный как инициатива студенческого активма Московского колледжа транспорта.
                     <br />Стэк: <code>JS</code>, <code>React</code>, <code>Node.js</code>, <code>Typescript</code>, <code>HTML5</code>, <code>CSS</code>.
                     <br />С нуля разработанный backend и frontend, личный кабинет, квизы и конкурсы.</div>
             />
 
 
-            <Card theme="red" time={new Date("08 19 2022")} tag1=<div>TSX + Node.Js</div> tag2=<a href="https://web.archive.org/web/20220827154459/https://xn--80aerhcl3be.xn--p1ai/"><i className="bi bi-box-arrow-up-right mx-2"></i>WebArchive</a> link="//активмкт.рф" linkText="Открыть сайт" title="Сайт студентческого актива МКТ" imageLink="/images/products/activemkt.png"
+            <Card theme="red" time={new Date("08 19 2022")} tag1=<div>TSX + Node.Js</div> tag2=<a href="https://web.archive.org/web/20220827154459/https://xn--80aerhcl3be.xn--p1ai/"><i className="bi bi-box-arrow-up-right mx-2"></i>WebArchive</a> link="//активмкт.рф" linkText="Открыть сайт" title="Сайт студентческого актива МКТ" imageLink={`${publicPath}/images/products/activemkt.png`}
                 text=<div>Сайт, разработанный как инициатива студенческого активма Московского колледжа транспорта.
                     <br />Стэк: <code>JS</code>, <code>React</code>, <code>Node.js</code>, <code>Typescript</code>, <code>HTML5</code>, <code>CSS</code>.
                     <br />С нуля разработанный backend и frontend, личный кабинет, квизы и конкурсы.</div>
@@ -99,13 +101,13 @@ function ReactComponent() {
 
             
 
-            <Card theme="green" time={new Date("04 25 2022")} tag1=<div>PHP + CMS Joomla</div> link="//github.com/AlexC-ux/EncryShare-Java" linkText="Открыть репозиторий" title="Мессенджер EncryShare" imageLink="/images/products/encryshare.png"
+            <Card theme="green" time={new Date("04 25 2022")} tag1=<div>PHP + CMS Joomla</div> link="//github.com/AlexC-ux/EncryShare-Java" linkText="Открыть репозиторий" title="Мессенджер EncryShare" imageLink={`${publicPath}/images/products/encryshare.png`}
                 text=<div>Сайт, разработанный для образовательной организации "Московский колледж транспорта".
                     <br />Стэк: <code>Java</code>, <code>PHP</code>.
                     <br />Мобильное приложение написано на Java, из функционала: регистрация в приложении, шифрование сообщений, создание групповых чатов. API написан на PHP.</div>
             />
 
-            <Card theme="blue" time={new Date("01 31 2022")} tag1=<div>PHP + CMS Joomla</div> tag2=<a href="//web.archive.org/web/20220322150522/http://mkgt.ru/"><i className="bi bi-box-arrow-up-right mx-2"></i>WebArchive</a> link="//mkgt.ru" linkText="Открыть сайт" title="Сайт Московского колледжа транспорта" imageLink="/images/products/mkgt.png"
+            <Card theme="blue" time={new Date("01 31 2022")} tag1=<div>PHP + CMS Joomla</div> tag2=<a href="//web.archive.org/web/20220322150522/http://mkgt.ru/"><i className="bi bi-box-arrow-up-right mx-2"></i>WebArchive</a> link="//mkgt.ru" linkText="Открыть сайт" title="Сайт Московского колледжа транспорта" imageLink={`${publicPath}/images/products/mkgt.png`}
                 text=<div>Сайт, разработанный для образовательной организации "Московский колледж транспорта".
                     <br />Стэк: <code>JS</code>, <code>PHP</code>, <code>HTML5</code>, <code>CSS</code>.
                     <br />Основная часть на базе CMS Joomla, доработанный функционал на php, внедрение и кастомизация шаблона под требования заказчика, вёрстка страниц, оптимизация до 85 баллов по GooglePageSpeed, оптимизация мобильной версии сайта.               </div>
