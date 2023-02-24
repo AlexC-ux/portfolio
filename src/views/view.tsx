b374fe92fe9f5cd83bb5e4155abac95346345197
AlexC-ux <eunv2004@gmail.com>
2023-02-25 00:40:55 +0300

    pay to play

--- src/views/view.tsx
+++ src/views/view.tsx
@@ -7,9 +7,9 @@ function ReactComponent() {
     document.body.classList.add("bg-dark")
 
 
-    let phoneRef = React.createRef < HTMLImageElement>();
-    let mailRef = React.createRef < HTMLImageElement>();
-    let photoRef= React.createRef < HTMLDivElement>();
+    let phoneRef = React.createRef<HTMLImageElement>();
+    let mailRef = React.createRef<HTMLImageElement>();
+    let photoRef = React.createRef<HTMLDivElement>();
 
     const copyToClipboard = (str: string) => {
         if (navigator && navigator.clipboard && navigator.clipboard.writeText)
@@ -17,25 +17,15 @@ function ReactComponent() {
         return Promise.reject('The Clipboard API is not available.');
     };
 
-    class ClickAgain{
-        private element;
-        private timeout;
-        constructor(element: any, timeout: number) {
-            this.element = element;
-            this.timeout = timeout;
-        }
-
-        public click() {
-            setTimeout(() => { this.element.click(); console.log(1) }, this.timeout)
-        }
-    }
-
     return (<>
 
-        <div className="w-100 bg-dark titlecontainer text-light" style={{ height: `200px` }}>
-         
+        <div className="w-100 bg-dark titlecontainer text-light"
+            style={{
+                position: "relative"
+            }}>
+
             <div className="row g-0 justify-content-center" style={{ paddingTop: `${(document.documentElement.clientHeight / 100 * 10)}px`, bottom: "0px" }}>
-                
+
                 <div className="col text-center fs-1">
                     <h1>Герасимов Александр Андреевич</h1>
                 </div>
@@ -46,9 +36,10 @@ function ReactComponent() {
                     <div onClick={(event) => {
                         if (!phoneRef.current!.getAttribute("aria-describedby")) {
                             setTimeout(() => { phoneRef.current!.click(); }, 2000);
-                        } copyToClipboard("+79955071469");  }}><i ref={phoneRef} className="bi bi-phone contactIcon" data-bs-custom-class="green-tooltip" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-trigger="focus click" data-bs-title="Номер телефона скопирован!"></i>
-                        
-                        </div>
+                        } copyToClipboard("+79955071469");
+                    }}><i ref={phoneRef} className="bi bi-phone contactIcon" data-bs-custom-class="green-tooltip" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-trigger="focus click" data-bs-title="Номер телефона скопирован!"></i>
+
+                    </div>
                 </div>
                 <div className="col">
                     <a href="https://github.com/AlexC-ux"><i className="bi bi-github text-light contactIcon"></i></a>
@@ -65,7 +56,10 @@ function ReactComponent() {
 
 
 
-        <div className="w-100 bg-dark my-4 mb-5">
+        <div className="w-100 bg-dark my-4 mb-5"
+            style={{
+                position: "relative"
+            }}>
             <h2 className="text-center text-light border-bottom my-2 pb-2 mx-auto" style={{ maxWidth: "250px" }}>Навыки</h2>
             <div className="row justify-content-center g-2 my-2 py-2">
                 <div className="col contactIcon">
@@ -90,7 +84,7 @@ function ReactComponent() {
                 </div>
                 <div className="col contactIcon">
                     <div className="imageContainer" style={{ height: "120px", width: "120px", backgroundImage: "url('')" }}>
-                        <img src="https://codehelper.com/wp-content/uploads/2018/10/javascript-icon.svg" style={{ width: "100%", height: "100%" }} />
+                        <img src="https://zevensource.com/resource/img/shared/logo/tools/js.svg" style={{ width: "100%", height: "100%" }} />
                     </div>
                 </div>
                 <div className="col contactIcon">
@@ -112,36 +106,45 @@ function ReactComponent() {
         </div>
 
 
-        <div className="container bg-dark text-light mt-5">
+        <div className="container bg-dark text-light mt-5"
+            style={{
+                position: "relative"
+            }}>
             <h2 className="text-center border-bottom my-2 mb-4 pb-2 mx-auto" style={{ maxWidth: "250px" }}>Проекты</h2>
 
-            <Card theme="red" time={new Date("08 19 2022")} tag1=<div>TSX + Node.Js</div> tag2=<a href="https://web.archive.org/web/20220827154459/https://xn--80aerhcl3be.xn--p1ai/"><i className="bi bi-box-arrow-up-right mx-2"></i>WebArchive</a> link="//активмкт.рф" linkText="Открыть сайт" title="Сайт студентческого актива МКТ" imageLink="./images/products/activemkt.png"
-                text=<div>Сайт, разработанный как инициатива студенческого активма Московского колледжа транспорта.
+            <Card theme="red" time={new Date("02 25 2023")} tag1={<div>Next.js + React + TypeScript + Node.Js + Redis + Prisma.JS</div>} link="https://github.com/AlexC-ux/pay-to-play" linkText="Открыть github" title="Самописный форум" imageLink="./images/products/paytoplay.png"
+                text={<div>Этот форум был разработан для демонстрации навыков использования различных библиотек и фреймворков
+                    <br />Стэк: <code>JS</code>, <code>React</code>, <code>Node.js</code>, <code>Typescript</code>, <code>HTML5</code>, <code>CSS</code>, <code>Next.js</code>, <code>Redis</code>, <code>Prisma.JS</code>.
+                    <br />С нуля разработанный backend и frontend, личный кабинет, посты, комменты, лайки, аватарки и всё соответствующее.</div>}
+            />
+
+            <Card theme="red" time={new Date("08 19 2022")} tag1={<div>TypeScript + Node.Js</div>} tag2={<a href="https://web.archive.org/web/20220827154459/https://xn--80aerhcl3be.xn--p1ai/"><i className="bi bi-box-arrow-up-right mx-2"></i>WebArchive</a>} link="//активмкт.рф" linkText="Открыть сайт" title="Сайт студентческого актива МКТ" imageLink="./images/products/activemkt.png"
+                text={<div>Сайт, разработанный как инициатива студенческого активма Московского колледжа транспорта.
                     <br />Стэк: <code>JS</code>, <code>React</code>, <code>Node.js</code>, <code>Typescript</code>, <code>HTML5</code>, <code>CSS</code>.
-                    <br />С нуля разработанный backend и frontend, личный кабинет, квизы и конкурсы.</div>
+                    <br />С нуля разработанный backend и frontend, личный кабинет, квизы и конкурсы.</div>}
             />
 
 
-            <Card theme="green" time={new Date("04 25 2022")} tag1=<div>Java + PHP</div> link="//github.com/AlexC-ux/EncryShare-Java" linkText="Открыть репозиторий" title="Мессенджер EncryShare" imageLink="./images/products/encryshare.png"
-                text=<div>Мессенджер в котором можно создавать групповые чатф. Сообщения шифруются и хранятся локально.
+            <Card theme="green" time={new Date("04 25 2022")} tag1={<div>Java + PHP</div>} link="//github.com/AlexC-ux/EncryShare-Java" linkText="Открыть репозиторий" title="Мессенджер EncryShare" imageLink="./images/products/encryshare.png"
+                text={<div>Мессенджер в котором можно создавать групповые чаты. Сообщения шифруются и хранятся локально.
                     <br />Стэк: <code>Java</code>, <code>PHP</code>.
-                    <br />Мобильное приложение написано на Java, из функционала: регистрация в приложении, шифрование сообщений, создание групповых чатов. API написан на PHP.</div>
+                    <br />Мобильное приложение написано на Java, из функционала: регистрация в приложении, шифрование сообщений, создание групповых чатов. API написан на PHP.</div>}
             />
 
-            <Card theme="yellow" time={new Date("03 20 2022")} tag1=<div>С# + PHP</div> link="//github.com/AlexC-ux/EncryShare" linkText="Открыть репозиторий" title="Десктоп приложение EncryShare" imageLink="./images/products/encryshare.png"
-                text=<div>Клиент-сервер на C# для передачи сообщений и файлов в зашифрованном виде, прямое подключение по IP.
+            <Card theme="yellow" time={new Date("03 20 2022")} tag1={<div>С# + PHP</div>} link="//github.com/AlexC-ux/EncryShare" linkText="Открыть репозиторий" title="Десктоп приложение EncryShare" imageLink="./images/products/encryshare.png"
+                text={<div>Клиент-сервер на C# для передачи сообщений и файлов в зашифрованном виде, прямое подключение по IP.
                     <br />Стэк: <code>С#</code>, <code>PHP</code>.
-                    <br />Приложение может выступать как сервером, так и клиентом, соединение происходит по протоколу tcp от клиента к серверу напрямую. Реализовано шифрование всех передаваемых данных и передача файлов и текста.</div>
+                    <br />Приложение может выступать как сервером, так и клиентом, соединение происходит по протоколу tcp от клиента к серверу напрямую. Реализовано шифрование всех передаваемых данных и передача файлов и текста.</div>}
             />
 
-            <Card theme="blue" time={new Date("01 31 2022")} tag1=<div>PHP + CMS Joomla</div> tag2=<a href="//web.archive.org/web/20220322150522/http://mkgt.ru/"><i className="bi bi-box-arrow-up-right mx-2"></i>WebArchive</a> link="//mkgt.ru" linkText="Открыть сайт" title="Сайт Московского колледжа транспорта" imageLink="./images/products/mkgt.png"
-                text=<div>Сайт, разработанный для образовательной организации "Московский колледж транспорта".
+            <Card theme="blue" time={new Date("01 31 2022")} tag1={<div>PHP + CMS Joomla</div>} tag2={<a href="//web.archive.org/web/20220322150522/http://mkgt.ru/"><i className="bi bi-box-arrow-up-right mx-2"></i>WebArchive</a>} link="//mkgt.ru" linkText="Открыть сайт" title="Сайт Московского колледжа транспорта" imageLink="./images/products/mkgt.png"
+                text={<div>Сайт, разработанный для образовательной организации "Московский колледж транспорта".
                     <br />Стэк: <code>JS</code>, <code>PHP</code>, <code>HTML5</code>, <code>CSS</code>.
-                    <br />Основная часть на базе CMS Joomla, доработанный функционал на php, внедрение и кастомизация шаблона под требования заказчика, вёрстка страниц, оптимизация до 85 баллов по GooglePageSpeed, оптимизация мобильной версии сайта.               </div>
+                    <br />Основная часть на базе CMS Joomla, доработанный функционал на php, внедрение и кастомизация шаблона под требования заказчика, вёрстка страниц, оптимизация до 85 баллов по GooglePageSpeed, оптимизация мобильной версии сайта.               </div>}
             />
 
 
-            
+
         </div>
     </>
     );
